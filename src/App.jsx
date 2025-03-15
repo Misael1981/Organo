5b8599c80a5578dfbbf4ec3d61d74ffcc920f9d4
Misael Borges <ml_bbd@hotmail.com>
2025-03-15 10:08:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Components/Banner/Banner";
+import Formulario from "./Components/Formulario";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
